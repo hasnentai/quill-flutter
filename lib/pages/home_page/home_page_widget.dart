a3774cb032f15ea555f220b8d36dee7e11c30fce
Kevin <hello+vercel@kevintonb.com>
2025-09-29 12:32:54 +0530

    better css interpolated with html string

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -92,6 +92,9 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                 child: ElevatedButton(
                     child: Text('Download as HTML'),
                     onPressed: () {
+                      _model.htmlString =
+                          '''<style>table,td,th{border:1px solid #000;border-collapse:collapse}.ql-editor ol li{list-style-type:decimal;padding-left:0}.ql-editor ul li{list-style-type:disc;padding-left:0;margin:0}.ql-editor ul{margin:0}table tbody tr td p{margin:0;padding:0}p{margin:0;padding:0}</style>${_model.htmlString}''';
+                      safeSetState(() {});
                       final htmlContent = _model.htmlString ?? '';
                       final bytes = utf8.encode(htmlContent);
                       final blob = html.Blob([bytes], 'text/html');
